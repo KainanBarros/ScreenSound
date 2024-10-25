4293bc559ac44eaa1261c7cd3926dbb98b711443
KainanBarros <kainanabarros@gmail.com>
2024-10-25 01:31:18 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound/Album.cs
@@ -0,0 +1,27 @@
+﻿using ScreenSound;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Album
+{
+    private List<Musica> musicas = new List<Musica>();
+    public string Nome { get; set; }
+    public int DuracaoTotal => musicas.Sum(m => m.Duracao);
+
+
+    public void AdicionarMusica(Musica musica)
+    {
+        musicas.Add(musica);
+    }
+
+    public void ExibirMusicaDoAlbum()
+    {
+        Console.WriteLine($"Lista de música do álbum: {this.Nome}:\n");
+        foreach (var musica in musicas)
+        {
+            Console.WriteLine($"Música: {musica.Nome}");
+        }
+        Console.WriteLine($"\nPara ouvir este álbum inteiro você precisa de {this.DuracaoTotal}");
+    }
+}
---
+++ ScreenSound/Musica.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ScreenSound
+{
+    class Musica
+    {
+        public string Nome { get; set; }
+        public string Artista { get; set; }
+        public int Duracao { get; set; }
+        public bool Disponivel { get; set; }
+        public string DescricaoResumida => $"A Música {this.Nome} pertence a banda {this.Artista}";
+
+
+        public void ExibirFichaTecnica()
+        {
+            Console.WriteLine($"Nome: {this.Nome}");
+            Console.WriteLine($"Artista: {this.Artista}");
+            Console.WriteLine($"Duração: {this.Duracao}");
+            if (this.Disponivel == true)
+            {
+                Console.WriteLine("Disponível no plano");
+            }
+            else
+            {
+                Console.WriteLine("Adquira o plano Plus+");
+            }
+
+
+        }
+    }
+}
