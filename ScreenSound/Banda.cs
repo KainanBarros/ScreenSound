5817d0044bd7a273ed6ab79057aa898751d5b7dc
KainanBarros <kainanabarros@gmail.com>
2024-10-26 15:20:07 -0300

    Feat: Nova classe banda

---
+++ ScreenSound/Banda.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ScreenSound
+{
+    class Banda
+    {
+        private List<Album> albuns = new List<Album>();
+
+        public string Nome { get; set; }
+
+        public Banda(string nome)
+        {
+            this.Nome = nome;
+        }
+
+        public void AdicionarAlbum(Album album)
+        {
+            albuns.Add(album);
+        }
+
+        public void ExibirDiscografia()
+        {
+            Console.WriteLine($"Discografia da banda {this.Nome}");
+            foreach (Album album in albuns)
+            {
+                Console.WriteLine($"Álbum: {album.Nome} ({album.DuracaoTotal})");
+            }
+
+        }
+    }
+}
